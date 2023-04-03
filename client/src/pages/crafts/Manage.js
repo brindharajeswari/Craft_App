9e9b61b85e2e3221e8d4139288410501c0c69a3c
brindharajeswari <120037997+brindharajeswari@users.noreply.github.com>
2023-04-02 21:16:07 -0400

    manage page

--- client/src/pages/crafts/Manage.js
+++ client/src/pages/crafts/Manage.js
@@ -1,12 +1,53 @@
+import { useEffect, useState } from 'react';
 import Navbar from '../../components/Nav';
+import { getAllCrafts } from '../../services/craftService';
+import './manage.css';
 
 function Manage() {
 
+    const [crafts, setCraft] = useState([]);
+
+    const getCraft = async () => {
+
+        try {
+            const data = await getAllCrafts()
+            console.log(data)
+            setCraft(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    useEffect(() => {
+
+        getCraft()
+    }, []); 
 
 
     return (
         <>
             <Navbar/>
+            <div class="manage-container">
+            <table class="manage-table">
+                <thead>
+                    <tr className='manage-row'>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {crafts.map(craft =>
+                                <tr className='manage-row'>
+                                    <td>{craft.title}</td>
+                                    <td>{craft.category}</td>
+                                    <td>Edit | Delete</td>
+                                </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
         </>
     );
 }
